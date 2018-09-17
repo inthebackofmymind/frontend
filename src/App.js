34a4fcd0b46de54aa6162d6e48ac853823fc45c7
John Olson <inthebackofmymind@gmail.com>
2018-09-17 23:13:47 +0000

    Made an app text change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I'm not a bitch! 
+          I'm sooooooooo not a bitch! 
         </p>
       </div>
     );
